eb6ed3c2f937d7216d61c0422ae9d77cb9311ad1
Chueyi Vue <chuvue@ucdavis.edu>
2022-01-06 10:57:21 -0800

    home page created

---
+++ des157b/home-is-template/script.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    const button = document.querySelector('button');
+    const body = document.querySelector('body');
+    const banner = document.querySelector('#banner');
+    const sections = document.querySelectorAll('section')
+    let mode = 'dark';
+
+    button.addEventListener('click', function() {
+        if (mode === 'dark') {
+            body.className = 'switch';
+            banner.className = 'switch';
+            button.className = 'switch';
+            for (const section of sections) {
+                section.className = 'switch';
+            }
+            mode = 'light';
+        } else {
+            body.removeAttribute('class');
+            banner.removeAttribute('class');
+            button.removeAttribute('class');
+            for (const section of sections) {
+                section.removeAttribute('class');
+            }
+            mode = 'dark'
+        }
+    })
+})()
