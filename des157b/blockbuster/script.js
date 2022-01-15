d816d7c747108e2edae433c5a746e7cd7e46e86c
Chueyi Vue <chuvue@ucdavis.edu>
2022-01-14 19:34:20 -0800

    portal page updated

--- des157b/blockbuster/script.js
+++ des157b/blockbuster/script.js
@@ -3,5 +3,20 @@
     'use script';
     console.log('reading js');
 
-    const sonarSound = new Audio('media/sonar.mp3') 
+    const madeonButton = document.getElementById('madeonbutton')
+    const porterButton = document.getElementById('porterbutton')
+    const madeonVideo = document.getElementById('madeonvideo')
+    const porterVideo = document.getElementById('portervideo')
+
+    porterButton.addEventListener('click', function(){
+        madeonVideo.style.visibility = 'hidden';
+        porterVideo.style.visibility = 'visible';
+    })
+
+    madeonButton.addEventListener('click', function(){
+        porterVideo.style.visibility = 'hidden';
+        madeonVideo.style.visibility = 'visible';
+    })
+
+
 })();
