b155ffab0870cecca72ead04a673a3c53295ee8a
Chueyi Vue <chuvue@ucdavis.edu>
2022-01-15 15:49:39 -0800

    portal page updated

--- des157b/blockbuster/script.js
+++ des157b/blockbuster/script.js
@@ -11,20 +11,30 @@
 
     const madeonButton = document.getElementById('madeonbutton')
     const porterButton = document.getElementById('porterbutton')
+    const odeszaButton = document.getElementById('odeszabutton')
+    
 
     const madeonVideo = document.getElementById('madeonvideo')
     const porterVideo = document.getElementById('portervideo')
+    const odeszaVideo = document.getElementById('odeszavideo')
 
     const madeonText = document.getElementById('madeontext')
     const porterText = document.getElementById('portertext')
+    const odeszaText = document.getElementById('odeszatext')
 
     const madeonBox = document.getElementById('madeonbox')
     const porterBox = document.getElementById('porterbox')
+    const odeszaBox = document.getElementById('odeszabox')
 
     madeonButton.addEventListener('click', function(){
         porterText.style.visibility = 'hidden';
         porterVideo.style.visibility = 'hidden';
         porterBox.style.visibility = 'hidden';
+
+        odeszaText.style.visibility = 'hidden';
+        odeszaVideo.style.visibility = 'hidden';
+        odeszaBox.style.visibility = 'hidden';
+
         madeonVideo.style.visibility = 'visible';
         madeonText.style.visibility = 'visible';
         madeonBox.style.display = 'block';
@@ -34,9 +44,28 @@
         madeonVideo.style.visibility = 'hidden';
         madeonText.style.visibility = 'hidden';
         madeonBox.style.display = 'none';
+
+        odeszaText.style.visibility = 'hidden';
+        odeszaVideo.style.visibility = 'hidden';
+        odeszaBox.style.visibility = 'hidden';
+
         porterVideo.style.visibility = 'visible';
         porterText.style.visibility = 'visible';
         porterBox.style.visibility = 'visible';
+    })
+
+    odeszaButton.addEventListener('click', function(){
+        madeonVideo.style.visibility = 'hidden';
+        madeonText.style.visibility = 'hidden';
+        madeonBox.style.display = 'none';
+
+        porterText.style.visibility = 'hidden';
+        porterVideo.style.visibility = 'hidden';
+        porterBox.style.visibility = 'hidden';
+
+        odeszaVideo.style.visibility = 'visible';
+        odeszaText.style.visibility = 'visible';
+        odeszaBox.style.visibility = 'visible';
     })
 
     closer.addEventListener('mouseover', function(){
