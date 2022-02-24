1a9a4472a04621e6974acd7f8bc38cf5c95ca079
Chueyi Vue <chuvue@ucdavis.edu>
2022-02-23 23:23:18 -0800

    portal page updated

---
+++ des157b/final/development/version1/scripts.js
@@ -0,0 +1,90 @@
+(function () {
+
+    'use strict';
+    console.log('reading js');
+
+    // OVERLAY1    
+
+    document.getElementById('panel1').addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('overlay1').className = 'showing';
+        document.getElementById('panels-section').className = 'hidden';
+    })
+
+    document.getElementById('close1').addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('overlay1').className = 'hidden';
+        document.getElementById('panels-section').className = 'showing';
+    })
+
+    document.addEventListener('keydown', function(event){
+        if (event.key === 'Escape') {
+            document.getElementById('overlay1').className = 'hidden';
+            document.getElementById('panels-section').className = 'showing';
+        }
+    });
+
+    // OVERLAY2
+
+    document.getElementById('panel2').addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('overlay2').className = 'showing';
+        document.getElementById('panels-section').className = 'hidden';
+    })
+
+    document.getElementById('close2').addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('overlay2').className = 'hidden';
+        document.getElementById('panels-section').className = 'showing';
+    })
+
+    document.addEventListener('keydown', function(event){
+        if (event.key === 'Escape') {
+            document.getElementById('overlay2').className = 'hidden';
+            document.getElementById('panels-section').className = 'showing';
+        }
+    });
+
+    // OVERLAY3
+
+    document.getElementById('panel3').addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('overlay3').className = 'showing';
+        document.getElementById('panels-section').className = 'hidden';
+    })
+
+    document.getElementById('close3').addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('overlay3').className = 'hidden';
+        document.getElementById('panels-section').className = 'showing';
+    })
+
+    document.addEventListener('keydown', function(event){
+        if (event.key === 'Escape') {
+            document.getElementById('overlay3').className = 'hidden';
+            document.getElementById('panels-section').className = 'showing';
+        }
+    });
+
+    // OVERLAY4
+
+    document.getElementById('panel4').addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('overlay4').className = 'showing';
+        document.getElementById('panels-section').className = 'hidden';
+    })
+
+    document.getElementById('close4').addEventListener('click', function(event){
+        event.preventDefault();
+        document.getElementById('overlay4').className = 'hidden';
+        document.getElementById('panels-section').className = 'showing';
+    })
+
+    document.addEventListener('keydown', function(event){
+        if (event.key === 'Escape') {
+            document.getElementById('overlay4').className = 'hidden';
+            document.getElementById('panels-section').className = 'showing';
+        }
+    });
+    
+})();
