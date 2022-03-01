b3367e969ed68f48238ebea4e15977332d86345b
Chueyi Vue <chuvue@ucdavis.edu>
2022-03-01 00:39:48 -0800

    portal page updated

--- des157b/final/development/version2/scripts.js
+++ des157b/final/development/version2/scripts.js
@@ -3,6 +3,79 @@
     'use strict';
     console.log('reading js');
 
+    Parse.initialize("dXnQbCCMhIThm2mxW0ulzEwTtD51B67cp4rGzf0j","lt0vEry0LBasv2SVUYR7meCPPz8tVcU0m1I2ohO2"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+    Parse.serverURL = 'https://parseapi.back4app.com/'
+
+    const list = document.querySelector("ol")
+    // const inputs = document.querySelectorAll("#add-story input:not([type=submit])")
+
+
+    // SUBMIT
+    // document.getElementById('submit').addEventListener('click', function(event){
+    //     event.preventDefault();
+    //     addStory();
+    // })
+
+    // BACK4APP
+    async function display() {
+        const stories = Parse.Object.extend('Stories');
+        const query = new Parse.Query(stories);
+        const results = await query.ascending('firstname').find();
+        console.log(results);
+
+        results.forEach(function(eachStory){
+            const id = eachStory.id;
+            const title = eachStory.get('title');
+            const firstname = eachStory.get('firstname');
+            const lastname = eachStory.get('lastname');
+            const story = eachStory.get('story');
+
+            const theList = document.createElement("li");
+            theList.setAttribute("id", `r=${id}`);
+            theList.innerHTML = `
+            <div class = "gallery-box">
+            <p class = "gallery-content gallery-title">${title}</p>
+            <p class = "gallery-content gallery-name">${firstname} ${lastname}</p>
+            <p class = "gallery-content gallery-story">${story}</p>
+        </div>`
+
+        list.append(theList);
+        })
+    }
+    display();
+
+    // async function addStory() {
+    //     const newStory = {};
+
+    //     for (let i = 0; i<inputs.length; i++) {
+    //         let key = inputs[i].getAttribute('name');
+    //         let value = inputs[i].value;
+    //         newStory[key] = value;
+    //     }
+    //     if (newStory.title != "" && newStory.firstname != "" && newStory.lastname != ""&& newStory.story != ""){
+    //         const newStoryData = new Parse.Object('Stories');
+    //         newStoryData.set('title', newStory.title);
+    //         newStoryData.set('firstname', newStory.firstname);
+    //         newStoryData.set('lastname', newStory.lastname);
+    //         newStoryData.set('story', newStory.story);
+    //     }
+    //     else {
+    //         document.getElementById('overlay1').className = 'hidden';
+    //         document.getElementById('panels-section').className = 'showing';
+    //     }
+    //     try {
+    //         const result = await newStoryData.save();
+    //         document.getElementById('overlay1').className = 'hidden';
+    //         document.getElementById('panels-section').className = 'showing';
+    //         list.innerHTML = "";
+    //         display();
+    //     }
+    //     catch (error){
+    //         console.error('Error while creating friend:', error);
+    //     }
+    // }
+
+
     // OVERLAY1    
 
     document.getElementById('panel1').addEventListener('click', function(event){
--- des157b/friendslist/script.js
+++ des157b/friendslist/script.js
@@ -5,23 +5,62 @@
     'use strict';
 
     Parse.initialize("pnrlGNztiJOcEPDvGfYVWdofxdpMwQGOBx7ZgFTd","AJg1jerhdInXCqEeBcsgqtYN0ubrQGp7ICxvX87H");
-    Parse.serverURL = 'https://parseapi.back4app.com/'
+    Parse.serverURL = 'https://parseapi.back4app.com/';
 
     const newButton = document.getElementById('newbtn');
     const editButtons = document.querySelectorAll('.fa-edit');
     const addFriendForm = document.getElementById('add-friend');
     const editFriendForm = document.getElementById('edit-friend');
     const friendList = document.querySelector('main ol');
+    const inputs = document.querySelectorAll("#add-friend input:not([type=submit])");
 
+    // OPENS FORM
     newButton.addEventListener('click', function(event) {
         event.preventDefault();
         addFriendForm.className = 'add-friend-onscreen';
     });
 
+    // SUBMIT AND CLOSES FORM
     addFriendForm.addEventListener('submit', function(event){
         event.preventDefault();
-        addFriendForm.className = 'add-friend-offscreen';
+        // addFriendForm.className = 'add-friend-offscreen';
+        addFriend();
     });
+
+    async function addFriend() {
+        const newFriend = {};
+
+        for (let i=0; i<inputs.length; i++) {
+            let key = inputs[i].getAttribute('name');
+            let value = inputs[i].value;
+            newFriend[key] = value;
+        }
+
+        if (newFriend.fname != "" && newFriend.lname != "" && newFriend.email != "") {
+            const newFriendData = new Parse.Object('NewFriends');
+            newFriendData.set('fname', newFriend.fname);
+            newFriendData.set('lname', newFriend.lname);
+            newFriendData.set('email', newFriend.email);
+            newFriendData.set('facebook', newFriend.facebook);
+            newFriendData.set('twitter', newFriend.twitter);
+            newFriendData.set('instagram', newFriend.instagram);
+            newFriendData.set('linkedin', newFriend.linkedin);
+            try {
+                const result = await newFriendData.save();
+                console.log('friends made', result)
+                resetFormFields();
+                addFriendForm.className = "add-friend-offscreen";
+                friendList.innerHTML = '';
+                displayFriends(); 
+            }
+            catch (error) {
+                console.error('Error while creating friend:', error);
+            }
+        }
+        else {
+            addFriendForm.className = "add-friend-offscreen";
+        }
+    }
 
     for (let i = 0; i < editButtons.length; i++){
         editButtons[i].addEventListener('click', function(event){
@@ -35,27 +74,37 @@
         editFriendForm.className = 'edit-friend-offscreen'
     });
 
+    function resetFormFields() {
+        document.getElementById('fname').value = "";
+        document.getElementById("lname").value = "";
+        document.getElementById('email').value = "";
+        document.getElementById("fbook").value = "https://facebook.com";
+        document.getElementById('twitter').value = "https://twitter.com"
+        document.getElementById('insta').value = "https://instagram.com"
+        document.getElementById('linkedin').value = "https://linkedin.com"
+    }
+
     async function displayFriends() {
-        const friends = Parse.Object.extend('FriendsList');
+        const friends = Parse.Object.extend('NewFriends');
         const query = new Parse.Query(friends);
         const results = await query.ascending('lastname').find();
         console.log(results);
 
     results.forEach(function(eachFriend) {
         const id = eachFriend.id;
-        const lastname = eachFriend.get('lastname');
-        const firstname = eachFriend.get('firstname');
+        const lname = eachFriend.get('lname');
+        const fname = eachFriend.get('fname');
         const email = eachFriend.get('email');
         const facebook = eachFriend.get('facebook');
         const twitter = eachFriend.get('twitter');
         const instagram = eachFriend.get('instagram');
         const linkedin = eachFriend.get('linkedin');
-
         const theListItem = document.createElement('li');
+
         theListItem.setAttribute('id', `r-${id}`);
         theListItem.innerHTML = `
         <div class="name">
-        ${firstname} ${lastname}
+        ${fname} ${lname}
         </div>
         <div class="email">
             <i class="fas fa-envelope-square"></i> ${email}
@@ -69,8 +118,7 @@
         <i class="fas fa-edit" id = "e-${id}"></i>
         <i class="fas fa-times-circle" id = "d-${id}"></i>`;
 
-        friendList.append(theListItem);
-        
+        friendList.append(theListItem);        
         });
     }
 
