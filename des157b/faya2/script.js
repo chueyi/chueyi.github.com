a0af4676fc45aba9ac4bfe404bab6feadc6e967d
Chueyi Vue <chuvue@ucdavis.edu>
2022-05-24 21:11:42 -0700

    faya2 updated

--- des157b/faya2/script.js
+++ des157b/faya2/script.js
@@ -14,36 +14,61 @@
         document.getElementById('bluegradient2').className = 'hidden'
         document.getElementById('redgradient').className = 'hidden'
         document.getElementById('greengradient').className = 'hidden'
+        document.getElementById('farmercharacter1').className = 'hidden'
+        document.getElementById('farmercharacter2').className = 'hidden'
+        document.getElementById('droughtcharacter').className = 'hidden'
     }
 
     document.getElementById('button1').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById("intro-p").className = 'hidden';
         document.getElementById("story-p").className = 'showing';
+        document.getElementById('bluegradient1').className = 'hidden';
+        document.getElementById('greengradient').className = 'showing';
+        document.getElementById('watercharacter').className = 'hidden';
+        document.getElementById('farmercharacter1').className = 'showing'
+        document.getElementById('farmercharacter2').className = 'showing'
     })
 
     document.getElementById('button2').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById("intro-p").className = 'showing';
         document.getElementById("story-p").className = 'hidden';
+        document.getElementById('bluegradient1').className = 'showing';
+        document.getElementById('greengradient').className = 'hidden';
+        document.getElementById('watercharacter').className = 'showing';
+        document.getElementById('farmercharacter1').className = 'hidden'
+        document.getElementById('farmercharacter2').className = 'hidden'
     })
 
     document.getElementById('button3').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById("story-p").className = 'hidden';
         document.getElementById("goals-p").className = 'showing';
+        document.getElementById('greengradient').className = 'hidden';
+        document.getElementById('redgradient').className = 'showing'
+        document.getElementById('farmercharacter1').className = 'hidden'
+        document.getElementById('farmercharacter2').className = 'hidden'
+        document.getElementById('droughtcharacter').className = 'showing'
     })
 
     document.getElementById('button4').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById("story-p").className = 'showing';
         document.getElementById("goals-p").className = 'hidden';
+        document.getElementById('redgradient').className = 'hidden';
+        document.getElementById('greengradient').className = 'showing'
+        document.getElementById('farmercharacter1').className = 'showing'
+        document.getElementById('farmercharacter2').className = 'showing'
+        document.getElementById('droughtcharacter').className = 'hidden'
     })
 
     document.getElementById('button5').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById('homepage').className = 'hidden'
         document.getElementById('help').className = 'showing'
+        document.getElementById('redgradient').className = 'hidden'
+        document.getElementById('bluegradient1').className = 'showing'
     })
 
     document.getElementById('link1').addEventListener('click', function(event){
@@ -53,6 +78,9 @@
         document.getElementById('leftsection').className = 'bluegradient'
         document.getElementById('contact').className = 'hidden'
         document.getElementById('help').className = 'hidden'
+        document.getElementById('bluegradient1').className = 'showing';
+        document.getElementById('greengradient').className = 'hidden';
+        document.getElementById('redgradient').className = 'hidden';
     })
 
     document.getElementById('link2').addEventListener('click', function(event){
@@ -62,6 +90,9 @@
         document.getElementById('leftsection').className = 'bluegradient'
         document.getElementById('contact').className = 'hidden'
         document.getElementById('help').className = 'hidden'
+        document.getElementById('bluegradient1').className = 'showing';
+        document.getElementById('greengradient').className = 'hidden';
+        document.getElementById('redgradient').className = 'hidden';
     })
 
     document.getElementById('link3').addEventListener('click', function(event){
@@ -70,6 +101,9 @@
         document.getElementById('contact').className = 'hidden'
         document.getElementById('characterpanel').className = 'hidden'
         document.getElementById('homepage').className = 'hidden'
+        document.getElementById('bluegradient1').className = 'showing';
+        document.getElementById('greengradient').className = 'hidden';
+        document.getElementById('redgradient').className = 'hidden';
     })
 
     document.getElementById('link4').addEventListener('click', function(event){
@@ -78,12 +112,10 @@
         document.getElementById('contact').className = 'showing'        
         document.getElementById('help').className = 'hidden'
         document.getElementById('homepage').className = 'hidden'
+        document.getElementById('bluegradient1').className = 'showing';
+        document.getElementById('greengradient').className = 'hidden';
+        document.getElementById('redgradient').className = 'hidden';
     })
-
-    // document.getElementById('link5').addEventListener('click', function(event){
-    //     event.preventDefault();
-    //     document.getElementById('characterpanel').className = 'hidden'
-    // })
     
     document.getElementById('drought').addEventListener('mouseover', function(event){
         event.preventDefault();
