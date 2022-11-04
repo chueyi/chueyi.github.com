5e55b14d371f6565337d3d33757f25591d0044dc
Chueyi Vue <chuvue@ucdavis.edu>
2022-11-04 15:43:04 -0700

    new update

--- script.js
+++ script.js
@@ -5,7 +5,8 @@
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
     const header = document.querySelector('header');
-    const sections = document.querySelectorAll('section')
+    const sections = document.querySelectorAll('section');
+    const buttonweb = document.querySelector('button-web');
     let mode = 'dark';
 
     const switchSound  = new Audio('sound/switch.mp3');
@@ -32,5 +33,37 @@
         }
         switchSound.play();
     })
-    
+
+    document.getElementById('button-web').addEventListener('click', function(event){
+        event.preventDefault();
+        document.querySelector('#illustration').className = 'shrink'
+        document.querySelector('#graphicdesign').className = 'shrink'   
+        document.querySelector('#nav-web').className = 'showing'
+        document.querySelector('#nav-illustration').className = 'hidden'
+        document.querySelector('#nav-graphicdesign').className = 'hidden'
+        document.querySelector('#webdesign').className = 'expand'
+    });
+
+    document.getElementById('button-illustration').addEventListener('click', function(event){
+        event.preventDefault();
+        document.querySelector('#nav-web').className = 'hidden'
+        document.querySelector('#nav-illustration').className = 'showing'
+        document.querySelector('#nav-graphicdesign').className = 'hidden'
+        document.querySelector('#webdesign').className = 'shrink'
+        document.querySelector('#illustration').className = 'expand'
+        document.querySelector('#graphicdesign').className = 'shrink'
+        document.querySelector('#picture1').className = 'expand-picture'
+        document.querySelector('#picture2').className = 'expand-picture'
+    });
+
+    document.getElementById('button-graphicdesign').addEventListener('click', function(event){
+        event.preventDefault();
+        document.querySelector('#nav-web').className = 'hidden'
+        document.querySelector('#nav-illustration').className = 'hidden'
+        document.querySelector('#nav-graphicdesign').className = 'showing'
+        document.querySelector('#webdesign').className = 'shrink'
+        document.querySelector('#illustration').className = 'shrink'
+        document.querySelector('#graphicdesign').className = 'expand'
+    });
+
 })()
